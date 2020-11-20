e12db2a9085ba0a10977263ecfafd39db5ac0a33
Hermann Luca <lhermann@student.42.fr>
2020-11-20 16:43:00 +0100

    refacto interfaces.ts for be more generic

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -7,9 +7,23 @@ const oldSuperstrada = {
   years: new Date(),
   broken: false,
   summary(): string {
-    return `This ${this.name} was build in ${this.years} years.`;
+    return `
+      This ${this.name} was build in ${this.years} years.
+      This moto is broken ? ${this.broken}`;
   }
 };
+
+const beer = {
+  color: 'yellow',
+  carbonated: true,
+  sugar: 12,
+  summary(): string {
+    return `
+      This beer is ${this.color}. 
+      It's a carbonated beer? ${this.carbonated}. 
+      It's contain ${this.sugar}g of sugar. `
+  }
+}
 
 // this object don't fit to the interface because it's miss the summary function
 const oldGsxr = {
@@ -23,6 +37,7 @@ const printSummary = (item: Reportable) => {
 };
 
 printSummary(oldSuperstrada);
+printSummary(beer);
 //printVehicle(oldGsxr);
 // Argument of type '{ name: string; years: string; broken: boolean; }' is not assignable to parameter of type 'Reportable'.
 // Property 'summary' is missing in type '{ name: string; years: string; broken: boolean; }' but required in type 'Reportable'
