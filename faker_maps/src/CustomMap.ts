a06db96b765e26d0da4983fc4e79851dd015e2d1
Hermann Luca <lhermann@student.42.fr>
2020-11-25 09:20:28 +0100

    add a generic function for add marker on the map

--- faker_maps/src/CustomMap.ts
+++ faker_maps/src/CustomMap.ts
@@ -1,6 +1,12 @@
+interface IMappable {
+  location: {
+    lat: number,
+    lng: number
+  }
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
-  public googleMarker: google.maps.Marker;
 
   constructor(public elementId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(elementId), {
@@ -12,7 +18,13 @@ export class CustomMap {
     });
   }
 
-  public addMarker() {
-    this.googleMarker.setPosition
+  addMarker(mappable: IMappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    })
   }
 }
