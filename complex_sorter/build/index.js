545ab36128988fe39b9c93e00af8951203f5aaff
Hermann Luca <lhermann@student.42.fr>
2020-11-28 14:32:08 +0100

    Work on bubble sort method. Remove unused npm srcipt from few projects.

--- complex_sorter/build/index.js
+++ complex_sorter/build/index.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log('hello world!');
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        // destructuring collection.length with es6 destructuring features.
+        const { length } = this.collection;
+        // first for loop itearate on the whole collection.
+        for (let i = 0; i < length; i++) {
+            // second for loop itearate on the collection - the last item because we know he is the greater one so we don't resort it'.
+            for (let j = 0; j < length - i - 1; j++) {
+                // if loop check if the left item is greater than the right item if yes we swap them.
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // classic swap method desing for array
+                    const tmpLeftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = tmpLeftHand;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 3, -5, 0, 54, 2, -67]);
+// const sorter = new Sorter(['w', 'a', 'c', 'b',]);
+sorter.sort();
+console.log(sorter.collection);
