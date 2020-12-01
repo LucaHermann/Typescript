a3052ffb49a54bbea8ee6a296eb020efb23f1e9e
Hermann Luca <lhermann@student.42.fr>
2020-12-01 11:05:59 +0100

    use fs to read the file

--- csv_reader/src/index.ts
+++ csv_reader/src/index.ts
@@ -1,1 +1,12 @@
 import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf8'
+  })
+  .split('\n')
+  .map((matche: string): string[] => {
+    return matche.split(',');
+  });
+
+console.log(matches);
