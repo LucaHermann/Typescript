3e62863fea1c3446893fcd0bf13fc8719431e36a
Hermann Luca <lhermann@student.42.fr>
2020-12-02 15:58:27 +0100

    details on index.t for differents approach of code.

--- csv_reader/build/index.js
+++ csv_reader/build/index.js
@@ -3,11 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./composition/MatchReader");
 const CsvFileReader_1 = require("./composition/CsvFileReader");
 const MatchResult_1 = require("./MatchResult");
+// Inheritance index
+// const reader = new MatchReader('football.csv');
+// reader.read()
+// Composition index
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 let manUnitedWins = 0;
 let manUnitedDraw = 0;
+// inheritance for loop
+// for (let match of reader.data) {
+//   /*some code ...*/
+// }
+//composition for loop
 for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
--- csv_reader/src/index.ts
+++ csv_reader/src/index.ts
@@ -2,14 +2,24 @@ import { MatchReader } from './composition/MatchReader';
 import { CsvFileReader } from './composition/CsvFileReader';
 import { MatchResult } from './MatchResult';
 
+// Inheritance index
+// const reader = new MatchReader('football.csv');
+// reader.read()
+
+// Composition index
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
-
 matchReader.load();
 
 let manUnitedWins = 0;
 let manUnitedDraw = 0;
 
+// inheritance for loop
+// for (let match of reader.data) {
+//   /*some code ...*/
+// }
+
+//composition for loop
 for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
