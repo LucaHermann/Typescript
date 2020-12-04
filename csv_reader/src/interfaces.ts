8111126f2813edbc4c6940bb8bc4e671a255c9ee
Hermann Luca <lhermann@student.42.fr>
2020-12-04 10:31:27 +0100

    add 2 interface in interfaces. these 2 interfarce are for the reporting of our app.

--- csv_reader/src/interfaces.ts
+++ csv_reader/src/interfaces.ts
@@ -1,8 +1,15 @@
+import { MatchData } from "./MatchData";
+import { MovieData } from "./MovieData";
+
 export interface IDataReader {
   read(): void,
   data: string[][]
 }
 
 export interface IAnalyzer {
+  run(list: MatchData[] | MovieData[]): string;
+}
 
+export interface IReportTarget {
+  print(report: string): void;
 }
